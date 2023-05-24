cd37695b4f3f8b856483fcde9943a82468adef25
Abdourahamane Condé <douraconde224@gmail.com>
2023-05-24 21:27:00 +0200

    Creating ApiResponse model. Every Call to our API will receive a response of ApiResponse type.

---
+++ Models/ApiResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class ApiResponse
+    {
+        HttpStatusCode statusCode;
+        bool IsSuccess=true;
+        object result;
+        public List<String> ErrorMessage { get; set; } = new List<String>();
+    }
+}
