e217c05b37936baa5f2dd76307c7ba09d210c858
Abdourahamane Condé <douraconde224@gmail.com>
2023-06-14 20:10:17 +0200

    added Base Service

--- RespApiProject2_Consumer/Service/IService/IBaseService.cs
+++ RespApiProject2_Consumer/Service/IService/IBaseService.cs
@@ -1,7 +1,11 @@
-﻿namespace RespApiProject2_Consumer.Service.IService
+﻿using RespApiProject2_Consumer.Models;
+
+namespace RespApiProject2_Consumer.Service.IService
 {
     public interface IBaseService
     {
-        
+        public ApiResponse apiResponse { get; set; }
+        public Task<T> SendHttpRequestAsync<T>(ApiRequest apiRequest);
+
     }
 }
